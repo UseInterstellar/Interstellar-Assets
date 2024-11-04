fdf85ed3772492b697f8a9032a0839d90f648e8b
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-11-04 02:52:42 +0000

    1.0.6 - Rename images across both files

--- rename.py
+++ rename.py
@@ -4,38 +4,52 @@ import random
 import string
 
 def generate_random_filename(extension, length=10):
-    length = max(10, min(length, 40)) 
+    length = max(10, min(length, 40))
     random_name = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
     return f"{random_name}.{extension}"
 
-def rename_icons_and_update_json(json_file_path):
+def preload_name_map(json_files, name_map):
+    for json_file in json_files:
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+            for entry in data:
+                if 'image' in entry:
+                    old_image_name = os.path.basename(entry['image'])
+                    if old_image_name not in name_map:
+                        name_map[old_image_name] = None  
+
+def rename_icons_and_update_json(json_file_path, name_map, icon_directory):
     with open(json_file_path, 'r') as f:
         data = json.load(f)
-
-    icon_directory = './icons/'
 
     for entry in data:
         if 'image' in entry:
             old_image_path = entry['image']
             old_image_name = os.path.basename(old_image_path)
 
-            extension = old_image_name.split('.')[-1]
+            if name_map[old_image_name] is None:
+                extension = old_image_name.split('.')[-1]
+                new_image_name = generate_random_filename(extension, random.randint(10, 40))
+                old_image_full_path = os.path.join(icon_directory, old_image_name)
+                new_image_full_path = os.path.join(icon_directory, new_image_name)
 
-            new_image_name = generate_random_filename(extension, random.randint(10, 40))
+                if os.path.exists(old_image_full_path):
+                    os.rename(old_image_full_path, new_image_full_path)
+                    name_map[old_image_name] = new_image_name  
 
-            old_image_full_path = os.path.join(icon_directory, old_image_name)
-            new_image_full_path = os.path.join(icon_directory, new_image_name)
-
-            if os.path.exists(old_image_full_path):
-                os.rename(old_image_full_path, new_image_full_path)
-                entry['image'] = f"/icons/{new_image_name}"
+            entry['image'] = f"/icons/{name_map[old_image_name]}"
 
     with open(json_file_path, 'w') as f:
         json.dump(data, f, indent=2)
 
     print(f"Icons renamed and {json_file_path} updated successfully!")
 
+name_map = {}
+icon_directory = './icons/'
+
 json_files = ['./json/apps.json', './json/games.json']
 
+preload_name_map(json_files, name_map)
+
 for json_file in json_files:
-    rename_icons_and_update_json(json_file)
+    rename_icons_and_update_json(json_file, name_map, icon_directory)
